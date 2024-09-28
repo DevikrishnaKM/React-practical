61a59af4bb1e4f1f473de07b7d716868ef0ae9f8
Devi krishna <devikrishna2345@gmail.com>
2024-09-28 11:38:02 +0530

    bg changer completed

--- bgChanger/src/App.jsx
+++ bgChanger/src/App.jsx
@@ -1,34 +1,48 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+ const [color, setColor] = useState('red')
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+     <div className='w-full h-screen duration-200' style={{ backgroundColor: color }}>
+      <div className='fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2'>
+        <div className='flex flex-wrap justify-center gap-3 shadow-lg bg-white px-2 rounded-xl'>
+          <button
+            onClick={() => setColor('red')}
+            className='w-10 h-7 rounded-xl bg-red-500 hover:bg-red-600'
+          ></button>
+          <button
+            onClick={() => setColor('blue')}
+            className='w-10 h-7 rounded-xl bg-blue-500 hover:bg-blue-600'
+          ></button>
+          <button
+            onClick={() => setColor('green')}
+            className='w-10 h-7 rounded-xl bg-green-500 hover:bg-green-600'
+          ></button>
+          <button
+            onClick={() => setColor('yellow')}
+            className='w-10 h-7 rounded-xl bg-yellow-500 hover:bg-yellow-600'
+          ></button>
+          <button
+            onClick={() => setColor('purple')}
+            className='w-10 h-7 rounded-xl bg-purple-500 hover:bg-purple-600'
+          ></button>
+          <button
+            onClick={() => setColor('orange')}
+            className='w-10 h-7 rounded-xl bg-orange-500 hover:bg-orange-600'
+          ></button>
+          <button
+            onClick={() => setColor('black')}
+            className='w-10 h-7 rounded-xl bg-black hover:bg-gray-900'
+          ></button>
+          <button
+            onClick={() => setColor('white')}
+            className='w-10 h-7 rounded-xl bg-white hover:bg-gray-400'
+          ></button>
+          </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+     </div>
   )
 }
 
